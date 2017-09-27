1ae37827d8496c63721a9c56bc8f8c16a395c6b5
Florent V <florent.vain@gmail.com>
2017-09-27 21:51:22 +0200

    Correction erreur de platform sur la sauvegarde
    Differenciation sur la création fonction de création de dossier entre windows et linux

--- src/save/sauvegarde.cpp
+++ src/save/sauvegarde.cpp
@@ -2,9 +2,13 @@
 
 int Sauvegarde::sauvegarderModele(Modele *modele)
 {
-    if (!mkdir("save", 0777));
+    #ifdef __MINGW32__
+    if (!mkdir("save"))
+    #elif __linux__
+    if (!mkdir("save", 0777))
+    #endif
     {
-        std::cout << "Dossier créé !" << std::endl;
+        std::cerr << "Dossier créé !" << std::endl;
     }
 
     TiXmlDocument doc("save/save.xml");
@@ -18,10 +22,11 @@ int Sauvegarde::sauvegarderModele(Modele *modele)
     TiXmlElement *stats = new TiXmlElement("statistiques");
     elem->LinkEndChild(stats);
 
-    stats->SetAttribute("Vie", "10");
-    stats->SetAttribute("Force", "10");
-    stats->SetAttribute("Vitesse", "10");
-    stats->SetAttribute("Intelligence", "10");
+    stats->SetAttribute("Niveau", std::to_string(modele->obtenirJoueur()->obtenirPersonnageJoueur()->obtenirNiveau()));
+    stats->SetAttribute("Vie", std::to_string(modele->obtenirJoueur()->obtenirPersonnageJoueur()->obtenirVie()));
+    stats->SetAttribute("Force", std::to_string(modele->obtenirJoueur()->obtenirPersonnageJoueur()->obtenirForce()));
+    stats->SetAttribute("Intelligence", std::to_string(modele->obtenirJoueur()->obtenirPersonnageJoueur()->obtenirIntelligence()));
+    stats->SetAttribute("Vitesse", std::to_string(modele->obtenirJoueur()->obtenirPersonnageJoueur()->obtenirVitesse()));
 
     doc.SaveFile("save/save.xml");
 
