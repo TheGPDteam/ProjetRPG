90690b269b5e40de516f8c03d3385b4d2a04b231
Adri <adrien.boitelle@gmail.com>
2018-10-25 15:09:12 +0200

    Cliquable prêt

--- src/vue/interfaceutilisateur/cliquable.h
+++ src/vue/interfaceutilisateur/cliquable.h
@@ -3,24 +3,28 @@
 
 #include <string>
 #include "controleur/controleur.h"
-#include <SDL/SDL.h>
 
-//const std::string DEFAUT_SON_CLIC = "../rsc/audio/default_clic.wav";
+#include <SDL/SDL.h>
 
 class Cliquable
 {
 private:
     Controleur * m_controleur;
 
-    bool m_actif; // Si on peut cliquer le bouton
+    bool m_actif; // Si on peut cliquer l'objet
     void* m_action; // Pointeur vers la fonction à appeler pour réaliser l'action associée à l'objet cliquable
 
-    //std::string m_clic_son = DEFAUT_SON_CLIC; // Peut-être géré ailleurs dans le futur
+    virtual SDL_Rect * zone() const = 0;
+
 public:
     Cliquable() = delete;
     Cliquable(Controleur * controleur, void* action, bool actif);
-    bool estSurvole(SDL_Rect zone);
-    bool estClique(SDL_Rect zone);
+
+    virtual ~Cliquable() = default;
+
+    virtual bool contient(std::pair<int, int> &coord_souris) const;
+    virtual void clique() = 0;
+
 };
 
 #endif // CLIQUABLE_H
