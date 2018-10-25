d34ae00aa687c95b9c061b12238992ca038f369f
Leothaud Mathieu <mathieu.leothaud@etu.u-bordeaux.fr>
2018-10-25 23:14:11 +0200

    Ajout d'un bouton aux coordonnees non adaptees dans ecran histoire

--- src/vue/ecran/menu/ecranpremierejournee.cpp
+++ src/vue/ecran/menu/ecranpremierejournee.cpp
@@ -8,7 +8,19 @@ EcranPremiereJournee::EcranPremiereJournee(Controleur* controleur)
 {
    m_fondRecapitulatif = {0, 0, WIDTH_FENETRE_PRINCIPALE, HEIGHT_FENETRE_PRINCIPALE};
 
-    recupererHistoire();
+     SDL_Rect positionFeuilleSprite = initialiserRectangle(COORD_X_BOUTON_NORMAL, COORD_Y_BOUTON_NORMAL, WIDTH_BOUTON_NORMAL, HEIGHT_BOUTON_NORMAL);
+     SDL_Rect positionFenetreSuivant = initialiserRectangle((WIDTH_FENETRE_PRINCIPALE/2)-(WIDTH_BOUTON_NORMAL/2),
+                                                     (HEIGHT_FENETRE_PRINCIPALE/2)-(HEIGHT_BOUTON_NORMAL/2)+ 100,
+                                                     WIDTH_BOUTON_NORMAL, HEIGHT_BOUTON_NORMAL);
+     Sprite * bouton = new Sprite(SPRITES_PRINCIPAUX, positionFenetreSuivant, positionFeuilleSprite);
+
+     std::pair<int, int> coordB((WIDTH_FENETRE_PRINCIPALE/2)-(WIDTH_BOUTON_NORMAL/2)  , (HEIGHT_FENETRE_PRINCIPALE/2)-(HEIGHT_BOUTON_NORMAL/2)+ 100 );
+     std::pair<int, int> tailleB(WIDTH_BOUTON_NORMAL, HEIGHT_BOUTON_NORMAL);
+     SDL_Rect rect= {coordB.first, coordB.second, tailleB.first, tailleB.second};
+
+     ajoutBoutonDansMapDeBoutons(new Bouton("Suivant", rect, bouton, m_controleur, nullptr,
+                                            true, std::make_pair<float, float>(coordB.first+20,coordB.second+15), POLICE_COLLEGED), &ActionsBoutons::boutonChoixNom);
+     recupererHistoire();
     //A SUPPRIMER
     /*
     ajoutBoutonDansMapDeBoutons(new Bouton(Normal, true, "Suivant", POLICE_COLLEGED, 20,
@@ -56,7 +68,7 @@ void EcranPremiereJournee::afficherEcran(std::pair<int, int> coord_souris, SDL_S
     }
 
     //A SUPPRIMER
-    //afficherBoutons(coord_souris, fenetre_affichage);
+    afficherBoutons(coord_souris, fenetre_affichage);
 }
 
 
