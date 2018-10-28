0b2ca521923ea68670dd9435eaebc623f4f42df7
Leothaud Mathieu <mathieu.leothaud@etu.u-bordeaux.fr>
2018-10-28 20:30:57 +0100

    Rajout d'autres boutons

--- src/vue/ecran/menu/ecranaccueilcampement.cpp
+++ src/vue/ecran/menu/ecranaccueilcampement.cpp
@@ -24,6 +24,29 @@ EcranAccueilCampement::EcranAccueilCampement(Controleur* controleur)
     ajoutBoutonDansMapDeBoutons(new Bouton(Normal, true, "Deposer inventaire", POLICE_COLLEGED, 15, std::make_pair(WIDTH_FENETRE_PRINCIPALE - 290, HEIGHT_FENETRE_PRINCIPALE - 200), std::make_pair(WIDTH_BOUTON_NORMAL, HEIGHT_BOUTON_NORMAL), std::make_pair(WIDTH_FENETRE_PRINCIPALE - 275, HEIGHT_FENETRE_PRINCIPALE - 182)), &ActionsBoutons::boutonViderInventaireCampement);
     ajoutBoutonDansMapDeBoutons(new Bouton(Normal, true, "Liste objets", POLICE_COLLEGED, 15, std::make_pair(WIDTH_FENETRE_PRINCIPALE - 290, HEIGHT_FENETRE_PRINCIPALE - 300), std::make_pair(WIDTH_BOUTON_NORMAL, HEIGHT_BOUTON_NORMAL), std::make_pair(WIDTH_FENETRE_PRINCIPALE - 235, HEIGHT_FENETRE_PRINCIPALE - 282)), &ActionsBoutons::boutonListeObjet);
     */
+
+    const std::pair<int, int> coordB(WIDTH_FENETRE_PRINCIPALE - 290, HEIGHT_FENETRE_PRINCIPALE - 100);
+    const std::pair<int, int> coordB2(WIDTH_FENETRE_PRINCIPALE - 290, HEIGHT_FENETRE_PRINCIPALE - 200);
+    const std::pair<int, int> coordB3(WIDTH_FENETRE_PRINCIPALE - 290, HEIGHT_FENETRE_PRINCIPALE - 300);
+    const std::pair<int, int> tailleB(WIDTH_BOUTON_NORMAL, HEIGHT_BOUTON_NORMAL);
+
+    SDL_Rect positionFeuilleSprite = initialiserRectangle(COORD_X_BOUTON_NORMAL, COORD_Y_BOUTON_NORMAL, WIDTH_BOUTON_NORMAL, HEIGHT_BOUTON_NORMAL);
+    SDL_Rect positionFenetreRetourJeu = initialiserRectangle(coordB.first, coordB.second, tailleB.first, tailleB.second);
+    SDL_Rect positionFenetreDeposerInventaire = initialiserRectangle(coordB2.first, coordB2.second, tailleB.first, tailleB.second);
+    SDL_Rect positionFenetreListeObjets = initialiserRectangle(coordB3.first, coordB3.second, tailleB.first, tailleB.second);
+
+    Sprite * bouton = new Sprite(SPRITES_PRINCIPAUX, positionFenetreRetourJeu, positionFeuilleSprite);
+    Sprite * bouton2 = new Sprite(SPRITES_PRINCIPAUX, positionFenetreDeposerInventaire, positionFeuilleSprite);
+    Sprite * bouton3 = new Sprite(SPRITES_PRINCIPAUX, positionFenetreListeObjets, positionFeuilleSprite);
+    SDL_Rect rect = {coordB.first, coordB.second, tailleB.first, tailleB.second};
+    SDL_Rect rect2 = {coordB2.first, coordB2.second, tailleB.first, tailleB.second};
+    SDL_Rect rect3 = {coordB3.first, coordB3.second, tailleB.first, tailleB.second};
+    ajoutBoutonDansMapDeBoutons(new Bouton("Retour Jeu", rect, bouton, m_controleur, nullptr,
+                                           true, std::make_pair<float, float>(coordB.first+20,coordB.second+15), POLICE_COLLEGED), &ActionsBoutons::boutonJeuPrincipal);
+    ajoutBoutonDansMapDeBoutons(new Bouton("Deposer Inventaire", rect2, bouton2, m_controleur, nullptr,
+                                           true, std::make_pair<float, float>(coordB2.first+20,coordB2.second+15), POLICE_COLLEGED), &ActionsBoutons::boutonViderInventaireCampement);
+    ajoutBoutonDansMapDeBoutons(new Bouton("Liste objets", rect3, bouton3, m_controleur, nullptr,
+                                           true, std::make_pair<float, float>(coordB3.first+20,coordB3.second+15), POLICE_COLLEGED), &ActionsBoutons::boutonListeObjet);
 }
 
 void EcranAccueilCampement::afficherEcran(std::pair<int, int> coord_souris, SDL_Surface *fenetre_affichage)
@@ -37,7 +60,7 @@ void EcranAccueilCampement::afficherEcran(std::pair<int, int> coord_souris, SDL_
     m_consoDispo.afficher(fenetre_affichage);
 
     //A SUPPRIMER
-    //afficherBoutons(coord_souris, fenetre_affichage);
+    afficherBoutons(coord_souris, fenetre_affichage);
 }
 
 void EcranAccueilCampement::gestionDesEvenements(Controleur *controleur, bool &quitter_jeu, bool &clique_souris, std::pair<int, int> &coord_souris)
