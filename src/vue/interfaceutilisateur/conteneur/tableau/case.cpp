fe9ca476ce785fd927be544f14472da688caee67
Marc Cerutti <mcerutti@info-307-10x.iut.bx1>
2018-11-08 16:07:13 +0100

    Début commentaire

--- src/vue/interfaceutilisateur/conteneur/tableau/case.cpp
+++ src/vue/interfaceutilisateur/conteneur/tableau/case.cpp
@@ -2,8 +2,8 @@
 
 //!
 //! \brief Case
-//! \param rect
-//! \param donnee
+//! \param rect x, y, hauteur, largeur en pixel
+//! \param donnee la donnee de la case
 //!
 Case(SDL_Rect rect, Affichable * donnee)
     : Affichable(rect),
@@ -30,7 +30,7 @@ void Case::afficher(SDL_Surface *surface)
 }
 
 //!
-//! \brief Case::redimensionner
+//! \brief Case::redimensionner redimmensione la donnee
 //! \param m_rectangle
 //!
 void Case::redimensionner(SDL_Rect m_rectangle)
@@ -41,6 +41,7 @@ void Case::redimensionner(SDL_Rect m_rectangle)
 //!
 //! \brief Case::definirDonnee
 //! \param donnee
+//! Attention, la donnee precedente est efface
 //!
 void Case::definirDonnee(Affichable *donnee)
 {
