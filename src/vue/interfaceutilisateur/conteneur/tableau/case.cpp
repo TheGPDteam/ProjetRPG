4ac56e568def37aa7fb89a8ac48809a27c14d75d
Marc Cerutti <mcerutti@info-307-10x.iut.bx1>
2018-11-08 16:04:26 +0100

    Début commentaire

--- src/vue/interfaceutilisateur/conteneur/tableau/case.cpp
+++ src/vue/interfaceutilisateur/conteneur/tableau/case.cpp
@@ -1,5 +1,10 @@
 #include "case.h"
 
+//!
+//! \brief Case
+//! \param rect
+//! \param donnee
+//!
 Case(SDL_Rect rect, Affichable * donnee)
     : Affichable(rect),
       donne(donnee)
@@ -7,27 +12,46 @@ Case(SDL_Rect rect, Affichable * donnee)
     redimensionner(rect);
 }
 
+//!
+//! \brief Case::~Case
+//!
 Case::~Case()
 {
     delete m_donnee;
 }
 
+//!
+//! \brief Case::afficher
+//! \param surface
+//!
 void Case::afficher(SDL_Surface *surface)
 {
     m_donnee->afficher(surface);
 }
 
+//!
+//! \brief Case::redimensionner
+//! \param m_rectangle
+//!
 void Case::redimensionner(SDL_Rect m_rectangle)
 {
     donnee->redimensionner(rect);
 }
 
+//!
+//! \brief Case::definirDonnee
+//! \param donnee
+//!
 void Case::definirDonnee(Affichable *donnee)
 {
     delete m_donnee;
     m_donnee = donnee;
 }
 
+//!
+//! \brief Case::obtenirDonnee
+//! \return
+//!
 Affichable *Case::obtenirDonnee() const
 {
     return m_donnee;
