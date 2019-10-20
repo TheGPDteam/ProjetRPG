fc0d481922e8fa178cd7f230927a0f33d7bb7061
Guilbaud A <24843378+GuilbaudAnt@users.noreply.github.com>
2019-10-20 15:16:17 +0200

    ajoute GestionnaireRessource.h

---
+++ src/GestionnaireRessource.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <map>
+class GestionnaireRessource
+{
+private:
+	std::map<std::string, SDL_Surface*>* m_tabSurface;
+public:
+	GestionnaireRessource();
+  	~GestionnaireRessource();
+	
+	void dechargerSurface(char* nom);
+	void chargementSurface(std::string nom, std::string extension);
+	SDL_Surface* obtenirSurface(const char* nom);
+};
