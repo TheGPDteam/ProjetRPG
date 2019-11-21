a0f073801581f752e547668e94bdf1d00db28f7d
Guilbaud A <24843378+GuilbaudAnt@users.noreply.github.com>
2019-11-21 14:11:28 +0100

    ajout format

--- src/gestionnaireressource.h
+++ src/gestionnaireressource.h
@@ -3,17 +3,20 @@
 #include <iostream>
 #include <SDL.h>
 #include <SDL_image.h>
+#include <fstream>
 #include <map>
 
 class GestionnaireRessource
 {
 private:
+    const std::string REPERTOIRE = "../rsc/sprites/";
+    const std::string EXTENSION = ".bmp";
 	std::map<std::string, SDL_Surface*>* m_tabSurface;
 public:
 	GestionnaireRessource();
   	~GestionnaireRessource();
 	
 	void dechargerSurface(char* nom);
-	void chargementSurface(std::string nom, std::string extension);
+    void chargementSurface(std::string nom);
 	SDL_Surface* obtenirSurface(const char* nom);
 };
