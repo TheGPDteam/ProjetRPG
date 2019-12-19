ad967a954dea78ade1e2836668fc69405e86b34c
MightyCode <bazinmaxence@free.fr>
2019-12-19 15:13:57 +0100

    Première version du changement de map

---
+++ src/modele/environnement/carte/zonechangementzone.cpp
@@ -0,0 +1,6 @@
+#include "zonechangementmap.h"
+
+ZoneChangementMap::ZoneChangementMap()
+{
+
+}
---
+++ src/modele/environnement/carte/zonechangementzone.h
@@ -0,0 +1,11 @@
+#ifndef ZONECHANGEMENTMAP_H
+#define ZONECHANGEMENTMAP_H
+
+
+class ZoneChangementMap
+{
+public:
+    ZoneChangementMap();
+};
+
+#endif // ZONECHANGEMENTMAP_H
